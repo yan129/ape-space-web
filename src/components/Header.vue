b0997a4a3d407e9899e3d35a0123adeddf9fd4ff
yan <1291248490@qq.com>
2022-09-05 20:42:14 +0800

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header-wrapper">
+      <div class="header-content-wrapper">
+          adwa
+          <Button type="primary">Primary</Button>
+          <Button type="success">Success</Button>
+            <Button type="warning">Warning</Button>
+            <Button type="error">Error</Button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created(){
+        window.document.documentElement.setAttribute( "data-theme", "dark" );
+    },
+    mounted(){
+        // document.getElementsByTagName('body')[0].style.setProperty('--headerColor', '#fff');
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/scss/themeMixin.scss";
+
+    .header-wrapper {
+        height: 60px;
+        // background-color: $header-color;
+        @include background-color("header-background-color");
+        @include font-color("normal-font-color");
+        @include font-size("normal-font-size");
+        .header-content-wrapper{
+            max-width: 1500px;
+            height: 100%;
+            margin: 0 auto;
+        }
+    }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-wrapper">
+      <header-box></header-box>
+  </div>
+</template>
+
+<script>
+import headerBox from '@/components/Header';
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        headerBox
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .main-wrapper {
+
+    }
+</style>
